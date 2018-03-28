95efe53f048e0150a881f4cf8e6d0f696e298ac4
Abel Cabral <jcabral@id.uff.br>
2018-03-28 05:00:56 -0300

    3.2.1
    update login database

--- gerente/php/conexao.php
+++ gerente/php/conexao.php
@@ -1,10 +1,10 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 class Conexao{
-    private $usuario = "root";
-    private $senha = "";
-    private $caminho = "localhost";
-    private $banco = "republica";
+    private $usuario = "u211833621_abel";
+    private $senha = "TvLtHj05a9G9";
+    private $caminho = "mysql.hostinger.com.br";
+    private $banco = "u211833621_rpb";
     private $con;
     
     public function __construct(){
